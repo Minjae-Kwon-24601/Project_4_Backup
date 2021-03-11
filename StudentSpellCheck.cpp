e352bb6f7db3b5ab988af0e735ad3a1326a05da3
ProjectFAR <71264291+ProjectFAR@users.noreply.github.com>
2021-03-11 08:36:18 -0800

    Update StudentSpellCheck.cpp

--- StudentSpellCheck.cpp
+++ StudentSpellCheck.cpp
@@ -1,18 +1,69 @@
 #include "StudentSpellCheck.h"
 #include <string>
 #include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 SpellCheck* createSpellCheck()
 {
 	return new StudentSpellCheck;
 }
 
-StudentSpellCheck::~StudentSpellCheck() {
-	// TODO
+StudentSpellCheck::StudentSpellCheck()
+{
+
+}
+
+StudentSpellCheck::~StudentSpellCheck() 
+{
+
 }
 
 bool StudentSpellCheck::load(std::string dictionaryFile) {
-	return true; // TODO
+	ifstream infile(dictionaryFile);		// create an input file stream object
+	if (!infile)
+		return false;			// if the opening of the file fails return false
+	else
+	{
+		string s;
+		
+		while (getline(infile, s))				// as long as there are still lines to process, store them in s
+		{
+			for (int i = 0; i < s.size(); i++)
+			{
+				s[i] = tolower(s[i]);
+				if (!isalpha(s[i]) && s[i] != '\'')
+					s.erase(i, 1);
+			}
+
+			Node* p = &dummy;
+			for (int i = 0; i < s.size(); i++)
+			{
+				int j = 0;
+				for (; j < p->trie_DS.size(); j++)
+				{
+					if (p->trie_DS[j]->value == s[i])
+					{
+						break;
+					}
+				}
+				if (j == p->trie_DS.size())
+				{
+					p->trie_DS.push_back(new Node);
+					p = p->trie_DS[j];
+					p->value = s[i];
+				}
+				else
+					p = p->trie_DS[j];
+			}
+			p->trie_DS.push_back(new Node);
+			int last_pos = p->trie_DS.size() - 1;
+			p = p->trie_DS[last_pos];
+			p->value = '.';
+		}
+		return true;
+	}
 }
 
 bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions) {
