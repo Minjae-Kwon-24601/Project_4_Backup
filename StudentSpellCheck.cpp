7234ceca5b7b1f30ea11ddee2df0471d1786bb5b
ProjectFAR <71264291+ProjectFAR@users.noreply.github.com>
2021-03-11 18:18:02 -0800

    Update StudentSpellCheck.cpp

--- StudentSpellCheck.cpp
+++ StudentSpellCheck.cpp
@@ -17,10 +17,16 @@ StudentSpellCheck::StudentSpellCheck()
 
 StudentSpellCheck::~StudentSpellCheck() 
 {
-
+	Node* ptr;
+	for (int i = 0; i < dummy.trie_DS.size(); i++)
+	{
+		ptr = dummy.trie_DS[i];
+		clearWords(ptr);
+	}
 }
 
-bool StudentSpellCheck::load(std::string dictionaryFile) {
+bool StudentSpellCheck::load(std::string dictionaryFile) 
+{
 	ifstream infile(dictionaryFile);		// create an input file stream object
 	if (!infile)
 		return false;			// if the opening of the file fails return false
@@ -66,10 +72,124 @@ bool StudentSpellCheck::load(std::string dictionaryFile) {
 	}
 }
 
-bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions) {
-	return true; // TODO
+bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions) 
+{
+	suggestions.erase(suggestions.begin(), suggestions.end());
+
+	string s = word;
+	if (checkWord(s))
+		return true;
+	else
+		return false;
+	/*int count = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		s[i] = tolower(s[i]);
+		
+	}
+
+
+	return true; */
 }
 
-void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<SpellCheck::Position>& problems) {
-	// TODO
+void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<SpellCheck::Position>& problems) 
+{
+	problems.erase(problems.begin(), problems.end());
+
+	int start;
+	int end;
+	int char_count = 0;
+	int iterator;
+	string check;
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (isalpha(line[i]) || line[i] == '\'')
+		{
+			if (char_count == 0)
+			{
+				start = i;
+				iterator = i;
+				check += line[i];
+				char_count++;
+			}
+			else if (i - 1 == iterator)
+			{
+				check += line[i];
+				char_count++;
+				iterator++;
+			}
+			if (i + 1 == line.size())
+			{
+				end = start + char_count - 1;
+				if (!checkWord(check))	
+				{
+					Position pos;
+					pos.start = start;
+					pos.end = end;
+					problems.push_back(pos);
+				}
+			}
+		}
+		else if (char_count != 0)
+		{
+			end = start + char_count - 1;
+			if (!checkWord(check))
+			{
+				Position pos;
+				pos.start = start;
+				pos.end = end;
+				problems.push_back(pos);	
+			}
+			char_count = 0;
+			check = "";
+		}
+	}
+}
+
+void StudentSpellCheck::clearWords(Node* cur)
+{
+	if (cur->trie_DS.size() == 0)
+		return;
+
+	for (int i = 0; i < cur->trie_DS.size(); i++)
+		clearWords(cur->trie_DS[i]);
+
+	delete cur;
+}
+
+bool StudentSpellCheck::checkWord(std::string to_check)
+{
+	Node* ptr = &dummy;
+	bool flag = false;
+	int count = 0;
+	for (int i = 0; i < to_check.size(); i++)
+	{
+		char cur_letter = tolower(to_check[i]);
+		for (int j = 0; j < ptr->trie_DS.size(); j++)
+		{
+			if (cur_letter == ptr->trie_DS[j]->value)
+			{
+				ptr = ptr->trie_DS[j];
+				flag = true;
+				count++;
+				break;
+			}
+		}
+		if (!flag)
+			break;
+		else
+			flag = false;
+	}
+
+	if (count == to_check.size())
+	{
+		for (int j = 0; j < ptr->trie_DS.size(); j++)
+		{
+			if ('.' == ptr->trie_DS[j]->value)
+				return true;
+		}
+		return false;
+	}
+	else
+		return false;
 }
